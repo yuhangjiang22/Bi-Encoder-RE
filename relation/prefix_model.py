927865ceaf0596e4aefd6b15af876bc0842e6bfc
yuhangjiang22 <yuhang.jiang176@topper.wku.edu>
2024-04-30 14:46:41 -0400

    Minor changes

--- relation/prefix_model.py
+++ relation/prefix_model.py
@@ -96,11 +96,14 @@ class BEFRE(PreTrainedModel):
         self.config.num_hidden_layers = hf_config.num_hidden_layers
         self.config.pruned_heads = hf_config.pruned_heads
         self.dropout = nn.Dropout(config.hidden_dropout_prob)
-        self.layer_norm = BertLayerNorm(hf_config.hidden_size * 2)
+        self.layer_norm = BertLayerNorm(hf_config.hidden_size)
         self.logit_scale = torch.nn.Parameter(torch.ones([]) * np.log(1 / config.init_temperature))
         self.post_init()
         self.num_labels = config.num_labels
         self.classifier = nn.Linear(hf_config.hidden_size * 2, config.num_labels)
+
+        self.input_linear = nn.Linear(hf_config.hidden_size * 2, hf_config.hidden_size)
+        self.des_linear = nn.Linear(hf_config.hidden_size * 3, hf_config.hidden_size)
 
         self.n_layer = hf_config.num_hidden_layers
         self.n_head = hf_config.num_attention_heads
@@ -227,17 +230,29 @@ class BEFRE(PreTrainedModel):
         sub_output = torch.cat([a[i].unsqueeze(0) for a, i in zip(sequence_output, sub_idx)])
         obj_output = torch.cat([a[i].unsqueeze(0) for a, i in zip(sequence_output, obj_idx)])
         rep = torch.cat((sub_output, obj_output), dim=1)
+        # for soft prompt
+        rep = self.input_linear(rep)
+        # change the dimension of layer norm
         rep = self.layer_norm(rep)
 
         # batch_size x hidden_size*2
         rep = self.dropout(rep)
 
+
+
         des_sub_output = torch.cat([a[i].unsqueeze(0) for a, i in zip(description_sequence_output, descriptions_sub_idx)])
         des_obj_output = torch.cat([a[i].unsqueeze(0) for a, i in zip(description_sequence_output, descriptions_obj_idx)])
         des_rep = torch.cat((des_sub_output, des_obj_output), dim=1)
-        des_rep = self.layer_norm(des_rep)
+
+        # for cls soft prompt
+        cls_rep = torch.cat([a[0].unsqueeze(0) for a in sequence_output])
+        cls_rep = cls_rep.repeat_interleave(num_types, dim=0)
+
+        des_rep = torch.cat((des_rep, cls_rep), dim=1)
+        des_rep = self.des_linear(des_rep)
 
         # batch_size*num_types x hidden_size*2
+        des_rep = self.layer_norm(des_rep)
         des_rep = self.dropout(des_rep)
 
         results = []
