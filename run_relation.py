c2dfc7576b0210be6d545696651ae105ee42f4cf
yuhangjiang22 <yuhang.jiang176@topper.wku.edu>
2024-05-03 16:39:02 -0400

    Minor changes

--- run_relation.py
+++ run_relation.py
@@ -69,6 +69,25 @@ id2description = {0: ["there are no relations between the compound @subject@ and
                       "of, or both a substrate and product within the gene's associated biochemical pathways ."
                       ]}
 
+id2description = {0: ["no relation : there are no relations between @subject@ and @object@ ."],
+                  1: ["part of : $subject$ is a component or segment that is integral to the structure or composition "
+                      "of $object$ ."],
+                  2: ["used for : $subject$ is a tool or method applied to enhance or facilitate $object$ ."],
+                  3: ["feature of : $subject$ is a constituent part or characteristic of @object@ , functioning as a "
+                      "distinctive"
+                      "element within @object@ , and falls within the scope or area of expertise defined by the "
+                      "domain of @object@ ."],
+                  4: ["conjunction : $subject$ serves a role or purpose analogous to @object@ , often being used in "
+                      "conjunction"
+                      "with or incorporated into @object@ for complementary or similar functions . "],
+                  5: ["evaluate for : $subject$ is assessed or analyzed specifically to determine its suitability, "
+                      "effectiveness, or performance in relation to @object@ ."],
+                  6: ["hyponym of : $subject$ is a specific instance or category under the broader classification of "
+                      "@object@ , signifying that $subject$ is a subtype or a more specialized form within the general "
+                      "framework of @object@ ."],
+                  7: ["compare : $subject$ is compared in relation to @object@ , highlighting similarities and "
+                      "differences to understand their respective characteristics or performances ."]}
+
 # id2description = {0: "there are no relations between the compound @subject@ and gene @object@ .",
 #                   1: "the compound @subject@ has been identified to engage with the gene @object@ , manifesting as an "
 #                      "upregulator, activator,or indirect upregulator in its interactions .",
