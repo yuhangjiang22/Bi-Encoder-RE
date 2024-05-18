701e9efde8542627039a3b59fd168ded9489169f
yuhangjiang22 <yuhang.jiang176@topper.wku.edu>
2024-05-18 01:07:41 -0400

    Minor changes

--- relation/utils.py
+++ relation/utils.py
@@ -33,8 +33,8 @@ def generate_relation_data(entity_data, use_gold=False, context_window=0):
                            ['SequenceVariant', 'SequenceVariant'],
                            ['GeneOrGeneProduct', 'ChemicalEntity'],
                            ['GeneOrGeneProduct', 'GeneOrGeneProduct']]
+    scierc_entity_types = ['Method', 'OtherScientificTerm', 'Task', 'Generic', 'Material', 'Metric']
     semeval_entity_types = ['sub', 'obj']
-    # biored_entity_types = ['DiseaseOrPhenotypicFeature', 'SequenceVariant', 'GeneOrGeneProduct', 'ChemicalEntity']
     logger.info('Generate relation data from %s' % (entity_data))
     data = Dataset(entity_data)
 
@@ -140,55 +140,59 @@ def generate_relation_data(entity_data, use_gold=False, context_window=0):
                             captured.append([sub.span.text, obj.span.text])
                             captured.append([obj.span.text, sub.span.text])
 
-                    elif sub.label in semeval_entity_types and obj.label in semeval_entity_types:
-                        label = gold_rel.get((sub.span, obj.span), 'no_relation')
-                        if label == 'no_relation':
-                            continue
-                        if label == 'Other':
-                            label = 'no_relation'
+                    # elif sub.label in semeval_entity_types and obj.label in semeval_entity_types:
+                    #     label = gold_rel.get((sub.span, obj.span), 'no_relation')
+                    #     if label == 'no_relation':
+                    #         continue
+                    #     if label == 'Other':
+                    #         label = 'no_relation'
+                    #
+                    #     sample = {}
+                    #     sample['docid'] = doc._doc_key
+                    #     sample['id'] = '%s@%d::(%d,%d)-(%d,%d)' % (
+                    #         doc._doc_key, sent.sentence_ix, sub.span.start_doc, sub.span.end_doc, obj.span.start_doc,
+                    #         obj.span.end_doc)
+                    #     sample['relation'] = label
+                    #     sample['subj_start'] = sub.span.start_sent + sent_start
+                    #     sample['subj_end'] = sub.span.end_sent + sent_start
+                    #     sample['subj_type'] = sub.label
+                    #     sample['obj_start'] = obj.span.start_sent + sent_start
+                    #     sample['obj_end'] = obj.span.end_sent + sent_start
+                    #     sample['obj_type'] = obj.label
+                    #     sample['token'] = tokens
+                    #     sample['sent_start'] = sent_start
+                    #     sample['sent_end'] = sent_end
+                    #
+                    #     sent_samples.append(sample)
 
-                        sample = {}
-                        sample['docid'] = doc._doc_key
-                        sample['id'] = '%s@%d::(%d,%d)-(%d,%d)' % (
-                            doc._doc_key, sent.sentence_ix, sub.span.start_doc, sub.span.end_doc, obj.span.start_doc,
-                            obj.span.end_doc)
-                        sample['relation'] = label
-                        sample['subj_start'] = sub.span.start_sent + sent_start
-                        sample['subj_end'] = sub.span.end_sent + sent_start
-                        sample['subj_type'] = sub.label
-                        sample['obj_start'] = obj.span.start_sent + sent_start
-                        sample['obj_end'] = obj.span.end_sent + sent_start
-                        sample['obj_type'] = obj.label
-                        sample['token'] = tokens
-                        sample['sent_start'] = sent_start
-                        sample['sent_end'] = sent_end
 
-                        sent_samples.append(sample)
+                    # else:
+                    if sub.label in scierc_entity_types and obj.label in scierc_entity_types:
+                        if [sub.span, obj.span] not in captured:
+                            label = gold_rel.get((sub.span, obj.span), 'no_relation')
+                            if label == 'no_relation':
+                                rev_label = gold_rel.get((obj.span, sub.span), 'no_relation')
+                            if label in ['CONJUNCTION', 'COMPARE'] or rev_label in ['CONJUNCTION', 'COMPARE']:
+                                captured.append([sub.span, obj.span])
+                                captured.append([obj.span, sub.span])
+                                label = rev_label
+                            sample = {}
+                            sample['docid'] = doc._doc_key
+                            sample['id'] = '%s@%d::(%d,%d)-(%d,%d)' % (
+                                doc._doc_key, sent.sentence_ix, sub.span.start_doc, sub.span.end_doc, obj.span.start_doc,
+                                obj.span.end_doc)
+                            sample['relation'] = label
+                            sample['subj_start'] = sub.span.start_sent + sent_start
+                            sample['subj_end'] = sub.span.end_sent + sent_start
+                            sample['subj_type'] = sub.label
+                            sample['obj_start'] = obj.span.start_sent + sent_start
+                            sample['obj_end'] = obj.span.end_sent + sent_start
+                            sample['obj_type'] = obj.label
+                            sample['token'] = tokens
+                            sample['sent_start'] = sent_start
+                            sample['sent_end'] = sent_end
 
-
-                    else:
-                    # if sub.label in scierc_entity_types and obj.label in scierc_entity_types:
-                    #     if [sub.span, obj.span] not in captured:
-                        label = gold_rel.get((sub.span, obj.span), 'no_relation')
-                        # if label == 'no_relation':
-                        #     label = gold_rel.get((obj.span, sub.span), 'no_relation')
-                        sample = {}
-                        sample['docid'] = doc._doc_key
-                        sample['id'] = '%s@%d::(%d,%d)-(%d,%d)' % (
-                            doc._doc_key, sent.sentence_ix, sub.span.start_doc, sub.span.end_doc, obj.span.start_doc,
-                            obj.span.end_doc)
-                        sample['relation'] = label
-                        sample['subj_start'] = sub.span.start_sent + sent_start
-                        sample['subj_end'] = sub.span.end_sent + sent_start
-                        sample['subj_type'] = sub.label
-                        sample['obj_start'] = obj.span.start_sent + sent_start
-                        sample['obj_end'] = obj.span.end_sent + sent_start
-                        sample['obj_type'] = obj.label
-                        sample['token'] = tokens
-                        sample['sent_start'] = sent_start
-                        sample['sent_end'] = sent_end
-
-                        sent_samples.append(sample)
+                            sent_samples.append(sample)
 
                         # captured.append([sub.span, obj.span])
                         # captured.append([obj.span, sub.span])
