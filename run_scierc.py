188c6a580501d45c69c4f8093113acca06c2788e
yuhangjiang22 <yuhang.jiang176@topper.wku.edu>
2024-06-06 01:19:50 -0400

    Minor changes

--- run_scierc.py
+++ run_scierc.py
@@ -178,9 +178,8 @@ def convert_examples_to_features(examples, label2id, max_seq_length, tokenizer, 
                               (sublist if isinstance(sublist, list) else [sublist])]
         description_tokens.append(SEP)
 
-        # des_sub_idx = description_tokens.index(SUBJECT_START_NER)
-        # des_obj_idx = description_tokens.index(OBJECT_START_NER)
-        des_sub_idx, des_obj_idx = 0, 0
+        des_sub_idx = description_tokens.index(SUBJECT_START_NER)
+        des_obj_idx = description_tokens.index(OBJECT_START_NER)
         descriptions_sub_idx.append(des_sub_idx)
         descriptions_obj_idx.append(des_obj_idx)
 
@@ -223,18 +222,18 @@ def convert_examples_to_features(examples, label2id, max_seq_length, tokenizer, 
         for i, token in enumerate(example['token']):
             if i == example['subj_start']:
                 sub_idx = len(tokens)
-                # tokens.append(SUBJECT_START_NER)
+                tokens.append(SUBJECT_START_NER)
             if i == example['obj_start']:
                 obj_idx = len(tokens)
-                # tokens.append(OBJECT_START_NER)
+                tokens.append(OBJECT_START_NER)
             for sub_token in tokenizer.tokenize(token):
                 tokens.append(sub_token)
             if i == example['subj_end']:
                 sub_idx_end = len(tokens)
-                # tokens.append(SUBJECT_END_NER)
+                tokens.append(SUBJECT_END_NER)
             if i == example['obj_end']:
                 obj_idx_end = len(tokens)
-                # tokens.append(OBJECT_END_NER)
+                tokens.append(OBJECT_END_NER)
         tokens.append(SEP)
 
         subject = tokens[sub_idx:sub_idx_end + 1]
