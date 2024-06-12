36718d002afd3c01781c5039b385d1713c9c2102
yuhangjiang22 <yuhang.jiang176@topper.wku.edu>
2024-06-12 18:32:16 -0400

    Minor changes

--- relation/utils.py
+++ relation/utils.py
@@ -174,25 +174,25 @@ def generate_relation_data(entity_data, use_gold=False, context_window=0):
 
                         # captured.append([sub.span, obj.span])
                         # captured.append([obj.span, sub.span])
-                    else:
-                        label = gold_rel.get((sub.span, obj.span), 'no_relation')
-                        sample = {}
-                        sample['docid'] = doc._doc_key
-                        sample['id'] = '%s@%d::(%d,%d)-(%d,%d)' % (
-                            doc._doc_key, sent.sentence_ix, sub.span.start_doc, sub.span.end_doc, obj.span.start_doc,
-                            obj.span.end_doc)
-                        sample['relation'] = label
-                        sample['subj_start'] = sub.span.start_sent + sent_start
-                        sample['subj_end'] = sub.span.end_sent + sent_start
-                        sample['subj_type'] = sub.label
-                        sample['obj_start'] = obj.span.start_sent + sent_start
-                        sample['obj_end'] = obj.span.end_sent + sent_start
-                        sample['obj_type'] = obj.label
-                        sample['token'] = tokens
-                        sample['sent_start'] = sent_start
-                        sample['sent_end'] = sent_end
-
-                        sent_samples.append(sample)
+                    # else:
+                    #     label = gold_rel.get((sub.span, obj.span), 'no_relation')
+                    #     sample = {}
+                    #     sample['docid'] = doc._doc_key
+                    #     sample['id'] = '%s@%d::(%d,%d)-(%d,%d)' % (
+                    #         doc._doc_key, sent.sentence_ix, sub.span.start_doc, sub.span.end_doc, obj.span.start_doc,
+                    #         obj.span.end_doc)
+                    #     sample['relation'] = label
+                    #     sample['subj_start'] = sub.span.start_sent + sent_start
+                    #     sample['subj_end'] = sub.span.end_sent + sent_start
+                    #     sample['subj_type'] = sub.label
+                    #     sample['obj_start'] = obj.span.start_sent + sent_start
+                    #     sample['obj_end'] = obj.span.end_sent + sent_start
+                    #     sample['obj_type'] = obj.label
+                    #     sample['token'] = tokens
+                    #     sample['sent_start'] = sent_start
+                    #     sample['sent_end'] = sent_end
+                    #
+                    #     sent_samples.append(sample)
 
             max_sentsample = max(max_sentsample, len(sent_samples))
             samples += sent_samples
