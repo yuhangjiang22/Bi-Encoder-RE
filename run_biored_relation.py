a225761eedbdbf5a2566b5c193686ffb8e6d1586
yuhangjiang22 <yuhang.jiang176@topper.wku.edu>
2024-06-16 21:21:29 -0400

    Minor changes

--- run_biored_relation.py
+++ run_biored_relation.py
@@ -653,7 +653,7 @@ def main(args):
 
     # train set
     if args.do_train:
-        train_dataset, train_examples, train_nrel = generate_relation_data(args.train_file,
+        train_dataset, train_examples, train_nrel = generate_relation_data(args.train_file, task=args.task,
                                                                            context_window=args.context_window)
         if args.train_num_examples:
             # train_dataset = train_dataset[:args.train_num_examples]
@@ -662,14 +662,14 @@ def main(args):
     # dev set
     if (args.do_eval and args.do_train) or (args.do_eval and not (args.eval_test)):
         eval_dataset, eval_examples, eval_nrel = generate_relation_data(
-            os.path.join(args.entity_output_dir, args.entity_predictions_dev),
+            os.path.join(args.entity_output_dir, args.entity_predictions_dev), task=args.task,
             context_window=args.context_window)
         if args.eval_num_examples:
             eval_examples = eval_examples[:args.eval_num_examples]
     # test set
     if args.eval_test:
         test_dataset, test_examples, test_nrel = generate_relation_data(
-            os.path.join(args.entity_output_dir, args.entity_predictions_test),
+            os.path.join(args.entity_output_dir, args.entity_predictions_test), task=args.task,
             context_window=args.context_window)
 
 
