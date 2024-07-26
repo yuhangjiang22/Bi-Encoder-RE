c660a8c5805aaa22f7a381a774f0878fe83e810f
yuhangjiang22 <yuhang.jiang176@topper.wku.edu>
2024-07-26 16:14:01 -0400

    Minor changes

--- relation/utils.py
+++ relation/utils.py
@@ -484,6 +484,34 @@ def generate_relation_data(entity_data, context_window=0, task=None):
                                 captured.append([sub.span.text, obj.span.text])
                                 captured.append([obj.span.text, sub.span.text])
 
+                    if task == 'ddi':
+                        # if type_pair in biored_entity_pairs: # This task is only for BioRED
+                        if [sub.span.text, obj.span.text] not in captured:
+                            label = gold_rel.get((sub.span, obj.span), 'no_relation')
+                            if label == 'no_relation':
+                                label = gold_rel.get((obj.span, sub.span), 'no_relation')
+                            sample = {}
+                            sample['docid'] = doc._doc_key
+                            sample['id'] = '%s@%d::(%d,%d)-(%d,%d)' % (
+                                doc._doc_key, sent.sentence_ix, sub.span.start_doc, sub.span.end_doc,
+                                obj.span.start_doc,
+                                obj.span.end_doc)
+                            sample['relation'] = label
+                            sample['subj_start'] = sub.span.start_sent + sent_start
+                            sample['subj_end'] = sub.span.end_sent + sent_start
+                            sample['subj_type'] = sub.label
+                            sample['obj_start'] = obj.span.start_sent + sent_start
+                            sample['obj_end'] = obj.span.end_sent + sent_start
+                            sample['obj_type'] = obj.label
+                            sample['token'] = tokens
+                            sample['sent_start'] = sent_start
+                            sample['sent_end'] = sent_end
+
+                            sent_samples.append(sample)
+
+                            captured.append([sub.span.text, obj.span.text])
+                            captured.append([obj.span.text, sub.span.text])
+
                     if task == 'scierc':
                         if [sub.span, obj.span] not in captured:
                             label = gold_rel.get((sub.span, obj.span), 'no_relation')
