2789d23bf648b7d6a0407e83ecd70982c123aeb0
yuhangjiang22 <yuhang.jiang176@topper.wku.edu>
2024-07-26 20:07:43 -0400

    Minor changes

--- relation/utils.py
+++ relation/utils.py
@@ -546,7 +546,7 @@ def generate_relation_data(entity_data, context_window=0, task=None):
                             sample['sent_end'] = sent_end
 
                             sent_samples.append(sample)
-                    if not task:
+                    if task=='semeval':
                         label = gold_rel.get((sub.span, obj.span), 'no_relation')
                         if label == 'Other':
                             label = 'no_relation'
