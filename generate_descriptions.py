5ba9be902cd40c5de19ca1c3f4bbe55afcee9506
yuhangjiang22 <74789310+yuhangjiang22@users.noreply.github.com>
2025-03-18 09:27:30 -0400

    Update generate_descriptions.py

--- generate_descriptions.py
+++ generate_descriptions.py
@@ -64,6 +64,6 @@ if __name__ == "__main__":
     openai_key = '<openai_key>'
     generate_descriptions = {}
     for task in datasets:
-        generate_descriptions[task] = generate_description(task, openai_key)
+        generated_descriptions[task] = generate_description(task, openai_key)
     # dump to .py file for easy read.
-    dump_dict_to_py_file(generate_descriptions, 'shared/generated_descriptions.py')
+    dump_dict_to_py_file(generated_descriptions, 'shared/generated_descriptions.py')
